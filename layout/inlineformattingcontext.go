1d5ab1739d884282923448fc86efcac9616a9050
hfen <739731746@qq.com>
2026-07-22 19:41:41 +0800

    "fixIFCfastPathYBaselineBug"

--- layout/inlineformattingcontext.go
+++ layout/inlineformattingcontext.go
@@ -83,12 +83,11 @@ func (c *InlineFormattingContext) Layout(box *LayoutBox, state *LayoutState) {
 	}
 
 	// Fast path: when the box itself is a leaf text run (e.g. direct text child of
-	// a flex container), generate a single segment inline.
 	if box.Type == BoxTextRun && len(box.Children) == 0 && box.Text != "" {
 		w := measureText(box, box.Text)
-		baseline := contentY + vCenterOffset + ascent
+		y := contentY + vCenterOffset
 		box.TextSegments = []TextSegment{
-			{Start: 0, Len: len(box.Text), X: contentX, Y: baseline, Width: w, Height: ascent + descent, LineY: contentY, LineHeight: lineHeight},
+		{Start: 0, Len: len(box.Text), X: contentX, Y: y, Width: w, Height: ascent + descent, LineY: contentY, LineHeight: lineHeight},
 		}
 		if box.Rect.Height < lineHeight {
 			box.Rect.Height = lineHeight
