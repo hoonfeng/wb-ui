92ba27d12ff0baf5a645050c418a2a61d1283dac
hfen <739731746@qq.com>
2026-07-27 12:27:10 +0800

    "fix_textoverflow_clip_contentbox"

--- rendering/renderpipeline.go
+++ rendering/renderpipeline.go
@@ -180,7 +180,14 @@ func walkSubtreeExcluded(root RenderObject, excluded map[RenderObject]bool, info
 	if clipBox != nil && info != nil && info.canvas != nil {
 		info.canvas.Save()
 		pb := clipBox.PaddingBoxRect()
-		info.canvas.Clip(graphics.Rect{X: pb.X, Y: pb.Y, Width: pb.Width, Height: pb.Height})
+		if st := clipBox.Style(); st != nil && st.TextOverflow == style.TextOverflowEllipsis {
+			// text-overflow:ellipsis clips to content box (horizontally)
+			// so overflowing text is hidden before "..." is painted.
+			cb := clipBox.ContentBoxRect()
+			info.canvas.Clip(graphics.Rect{X: cb.X, Y: pb.Y, Width: cb.Width, Height: pb.Height})
+		} else {
+			info.canvas.Clip(graphics.Rect{X: pb.X, Y: pb.Y, Width: pb.Width, Height: pb.Height})
+		}
 		needsClipRestore = true
 	}
 
